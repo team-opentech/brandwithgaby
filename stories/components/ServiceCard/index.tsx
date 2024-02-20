f2beb2c1f9782e88f084112587cc04450c2006f4
Daniel Bermudez <team@lccopen.tech>
2024-02-20 13:18:21 -0400

    properties added

--- stories/components/ServiceCard/index.tsx
+++ stories/components/ServiceCard/index.tsx
@@ -1,70 +1,67 @@
 import {Button} from '../button';
 
-export const ServiceCard = () => {
+interface ServiceCard {
+  title: string;
+  subtitle: string;
+  servicesList: any;
+  price: string;
+  dues: string;
+  include: any;
+  time: string;
+}
+
+export const ServiceCard = ({
+  title,
+  subtitle,
+  servicesList,
+  price,
+  dues,
+  include,
+  time,
+}: ServiceCard) => {
   return (
     <div className="w-full max-w-[354px] rounded rounded-lg lg:max-w-[419px] h-full max-h-[919px] flex flex-col justify-center items-center">
-      <div className="w-full bg-black rounded-t-lg p-[2vw] flex flex-col justify-center items-start">
-        <h3 className="text-white"> Kickstart </h3>
-        <h4 className="text-white"> Branding Package</h4>
+      <div className="w-full bg-black rounded-t-lg p-[3vw] lg:p-[2vw] flex flex-col justify-center items-start">
+        <h3 className="text-white"> {title} </h3>
+        <h4 className="text-white"> {subtitle}</h4>
       </div>
-      <div className="w-full bg-white p-[2vw] border border-b-black flex flex-col justify-center items-start">
-        <p className="text-black leading-[35px]">30-Min Zoom Meeting</p>
-        <p className="text-black leading-[35px]">Branding</p>
-        <p className="text-black leading-[35px]">Logo</p>
-        <p className="text-black leading-[35px]">Brand Guidelines</p>
+      <div className="w-full bg-white p-[3vw] lg:p-[2vw] border border-b-black flex flex-col justify-center items-start">
+        {servicesList.map((service, index) => (
+          <p key={index} className="text-black leading-[35px]">
+            {service}
+          </p>
+        ))}
       </div>
-      <div className="w-full bg-white p-[2vw] flex flex-col justify-center items-start border border-b-black">
+      <div className="w-full bg-white p-[3vw] lgp-[2vw] flex flex-col justify-center items-start border border-b-black">
         <p className="text-black font-bold text-[26px] leading-[35px]">
-          $1,980.00
+          ${price}
         </p>
-        <p className="text-[#9B9B9B] leading-[35px]">
-          o 4 cuotas de $495.00 with afterpay
-        </p>
+        <p className="text-[#9B9B9B] leading-[35px]">{dues}</p>
       </div>
-      <div className="w-full bg-white p-[2vw] flex flex-col justify-center items-start border border-b-black">
+      <div className="w-full bg-white p-[3vw] lgp-[2vw] flex flex-col justify-center items-start border border-b-black">
         <p className="text-black">Incluye:</p>
-        <div className="w-full space-x-[25px] flex flex-row justify-start items-center">
-          <img src="/STAR-v2.svg" alt="*" />
-          <p className="leading-[25px]">
-            {' '}
-            <span className="text-black">Brand Guidelines PDF:</span>{' '}
-            <span className="text-[#9B9B9B]">
+        {include.map((item, i) => (
+          <div
+            key={i}
+            className="w-full space-x-[25px] flex flex-row justify-start items-center"
+          >
+            <img src="/STAR-v2.svg" alt="*" />
+            <p className="leading-[25px]">
               {' '}
-              Concepto de estilo de marca con logo, tipografías, colores.{' '}
-            </span>{' '}
-          </p>
-        </div>
-        <div className="w-full space-x-[25px] flex flex-row justify-start items-center">
-          <img src="/STAR-v2.svg" alt="*" />
-          <p className="leading-[25px]">
-            {' '}
-            <span className="text-black">Brand Guidelines PDF:</span>{' '}
-            <span className="text-[#9B9B9B]">
-              {' '}
-              Concepto de estilo de marca con logo, tipografías, colores.{' '}
-            </span>{' '}
-          </p>
-        </div>
-        <div className="w-full space-x-[25px] flex flex-row justify-start items-center">
-          <img src="/STAR-v2.svg" alt="*" />
-          <p className="leading-[25px]">
-            {' '}
-            <span className="text-black">Brand Guidelines PDF:</span>{' '}
-            <span className="text-[#9B9B9B]">
-              {' '}
-              Concepto de estilo de marca con logo, tipografías, colores.{' '}
-            </span>{' '}
-          </p>
-        </div>
+              <span className="text-black">{item.title}</span>{' '}
+              <span className="text-[#9B9B9B]">{item.description}</span>{' '}
+            </p>
+          </div>
+        ))}
       </div>
-      <div className="w-full bg-white p-[2vw] rounded-b-lg flex flex-col justify-center items-start space-y-[20px]">
+      <div className="w-full bg-white p-[3vw] lgp-[2vw] rounded-b-lg flex flex-col justify-center items-start space-y-[20px]">
         <div className="w-full space-x-[25px] flex flex-row justify-start items-center">
           <img src="/clock.svg" alt="*" />
-          <p className="text-black">¡Pack listo en 2 semanas!</p>
+          <p className="text-black">{time}</p>
         </div>
         <div className="w-full flex flex-col justify-center items-center">
           <Button
-            styles="hidden lg:flex justify-center bg-black text-white p-2 rounded rounded-full h-10 w-full max-w-[203px] border border-solid border-black"
+            styles="justify-center bg-black text-white p-2 rounded rounded-full h-10 w-full max-w-[203px] border border-solid border-black"
             label="¡Comencemos!"
           />
         </div>
--- stories/components/ServiceCard/servicecard.stories.tsx
+++ stories/components/ServiceCard/servicecard.stories.tsx
@@ -11,5 +11,47 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
-  args: {},
+  args: {
+    title: 'Kickstar',
+    subtitle: 'Branding Package',
+    servicesList: [
+      '30-Min Zoom Meeting',
+      'Branding',
+      'Logo',
+      'Brand Guidelines',
+    ],
+    price: '1,980.00',
+    dues: 'o 4 cuotas de $495.00 with afterpay',
+    include: [
+      {
+        title: '2 Opciones de logo.',
+        description: '',
+      },
+      {
+        title: '2 Rondas de revisiones de logo',
+        description: '',
+      },
+      {
+        title: 'Brand Guidelines PDF:',
+        description:
+          'Concepto de estilo de marca con logo, tipografías, colores.',
+      },
+      {
+        title: '3 piezas de diseño a definir.',
+        description:
+          '(business cards, thank you card, stickers, etiquetas, hojas membretadas, artes sencillos).',
+      },
+      {
+        title: 'Social Media Assets:',
+        description:
+          'Logo para perfil, cover para Facebook, Youtube o Linkedin.',
+      },
+      {
+        title: 'Entrega de Brand Kit:',
+        description:
+          'Logo, tipografía y piezas de diseños en  archivos editables.',
+      },
+    ],
+    time: '¡Pack listo en 2 semanas!',
+  },
 };
