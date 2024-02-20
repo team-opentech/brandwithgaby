54866bb60bb85664eca8f2426388145eda35ec62
Team Opentech <team@lccopen.tech>
2024-02-20 17:30:29 -0400

    component marquesina created

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ export default {
       Inter: ['Inter', 'serif'],
       Chakra: ['Chakra Petch', 'sans-serif'],
     },
+    extend: {
+      keyframes: {
+        marquee: {
+          '0%': {transform: 'translateX(0)'},
+          '100%': {transform: 'translateX(-100%)'},
+        },
+      },
+      animation: {
+        'marquee-infinite': 'marquee 50s linear infinite',
+      },
+    },
   },
   content: ['./app/**/*.{js,ts,jsx,tsx}', './stories/**/*.{js,ts,jsx,tsx}'],
   darkMode: ['class', '[data-mode="dark"]'],
