38fa044432c4af4cd10ea6260e17bb579515e4c0
Team Opentech <team@lccopen.tech>
2024-03-07 12:24:34 -0400

    Add width

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -22,7 +22,7 @@ export function Layout({children = null, footer, header}: LayoutProps) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className="w-[100%]">{children}</main>
       <Footer />
 
     </>
